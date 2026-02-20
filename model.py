35d25d61928b093f006f6a785bb28dbf569265dd
Saran Prabhu <164511536+SaranPrabhu07@users.noreply.github.com>
2026-02-20 22:26:00 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from google import genai
 
-# Pass the string directly as the api_key
+## Pass the string directly as the api_key
 client = genai.Client(api_key="AIzaSyBvNgRnSw0Y1fG6s3bWdvGpHYnQE1X_LeU")
 
 for m in client.models.list():
